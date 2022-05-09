bac09fff5a83203053d314a5edca1a10ea5427ea
Raddiya <raddiyasultana.612@gmil.com>
2022-05-09 23:17:22 +0600

    json web token i

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -7,7 +8,20 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
-
+const jwtVerify = async (req, res, next) => {
+    const authHeaders = req.headers.authorization
+    if (!authHeaders) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeaders.split(' ')[1]
+    jwt.verify(token, process.env.TOKEN_SECRATE, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 const run = async () => {
 
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bhhzy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -24,6 +38,13 @@ const run = async () => {
             const result = await cursor.toArray()
             res.send({ result });
         })
+        app.get('/myitems',jwtVerify, async (req, res) => {
+            const email =req.decoded.email;
+            console.log(email)
+            const cursor = phoneCollection.find({email})
+            const result = await cursor.toArray()
+            res.send({ result });
+        })
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
             const result = await phoneCollection.findOne({ _id: ObjectId(id) })
@@ -34,6 +55,13 @@ const run = async () => {
 
             const result = await phoneCollection.insertOne({ name, about, image, price, quantity, email, supplier })
             res.send({ result });
+        })
+        app.post('/login', async (req, res) => {
+            const {  email } = req.body;
+            const token = jwt.sign({ email },  process.env.TOKEN_SECRATE,{
+                expiresIn:'1d'
+            });
+            res.send({token});
         })
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
@@ -46,9 +74,6 @@ const run = async () => {
             const result = await phoneCollection.updateOne({ _id: ObjectId(id) }, { $set: { quantity } }, { upsert: true })
             res.send({ result });
         })
-
-
-
     }
     finally {
 
